bcae5e98719f98eafaa0b5d834d0ee73fca80b41
Ben-235 <bensonmuroki235@gmail.com>
2023-02-16 15:16:45 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
-# !
-/ bin / bash
-
 #include <stdio.h>
-
 /**
- * main-Entry point
- *
- *Description: 'script to print using puts'
- *
- * Return: Always 0(success)
- */
-
+* main - A program that prints a line with puts function
+* Return: 0 (success)
+*/
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
